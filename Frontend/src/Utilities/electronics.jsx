e48f523da97cac4c87c1679f61e1fd9b0e474b02
vimal004 <2004.vimal@gmail.com>
2024-05-31 21:01:41 +0530

    Commit 8

--- Frontend/src/Utilities/electronics.jsx
+++ Frontend/src/Utilities/electronics.jsx
@@ -1,4 +1,23 @@
+import { useEffect, useState } from "react";
+
 const Electronics = () => {
+  const [data, setdata] = useState({});
+
+  const fetchdata = async () => {
+    const response = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.89960&lng=80.22090&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    const jsondata = await response.json();
+    console.log("Fetched data:", jsondata);
+    setdata(jsondata?.cards[0] || []);
+  };
+
+  useEffect(() => {
+    fetchdata();
+  }, []);
+
+  useEffect(() => {
+    console.log("Updated data:", data);
+  }, [data]);
+
   return (
     <div className="">
       <h1>Electronics</h1>
