d318a2759b3d3070269920163ac8f75f68596988
vimal004 <2004.vimal@gmail.com>
2024-10-11 20:28:59 +0530

    Refactor package.json and ws.js files to add dependencies and setup Socket.IO server

---
+++ ws/ws.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// When a new client connects
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  // Listen for incoming chat messages
+  socket.on("message", (data) => {
+    console.log("Message from client:", data);
+
+    // Broadcast the message to all connected clients (or agents)
+    io.emit("message", data);
+  });
+
+  // Handle disconnection
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+const port=process.env.PORT || 8080;
+server.listen(port, () => {
+  console.log("Socket.IO server running on port 8080");
+});
