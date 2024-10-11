82f5e086242549d309042a893aa796be5da79e51
vimal004 <2004.vimal@gmail.com>
2024-10-11 20:45:47 +0530

    Refactor ws.js, package.json, and package-lock.json to add cors dependency and enable CORS

--- ws/ws.js
+++ ws/ws.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const cors = require("cors"); 
 
 const app = express();
+app.use(cors()); // Enable CORS
 const server = http.createServer(app);
 const io = new Server(server);
 
