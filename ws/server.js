a30afea3a35599ad5b89e0bbac71da736d2150ce
vimal004 <2004.vimal@gmail.com>
2024-10-13 14:52:30 +0530

    Refactor server.js to update Socket.IO server for customer support functionality

--- ws/server.js
+++ ws/server.js
@@ -19,6 +19,9 @@ const io = new Server(server, {
   },
 });
 
+let availableExecutives = []; // List of available customer care executives
+let clientsToExecutives = {}; // Map to track which client is connected to which executive
+
 // Root endpoint
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -26,17 +29,76 @@ app.get("/", (req, res) => {
 
 // Handle socket connection
 io.on("connection", (socket) => {
-  console.log("a user connected with id: " + socket.id);
+  console.log(`User connected: ${socket.id}`);
 
-  // Listen for "message" event
+  // Listen for "registerExecutive" to mark a user as a customer care executive
+  socket.on("registerExecutive", () => {
+    console.log(`Executive joined: ${socket.id}`);
+    availableExecutives.push(socket.id); // Add the executive to the available list
+  });
+
+  // Listen for "registerClient" to mark a user as a client
+  socket.on("registerClient", () => {
+    console.log(`Client joined: ${socket.id}`);
+    if (availableExecutives.length > 0) {
+      // Assign an executive to the client
+      const assignedExecutive = availableExecutives.shift(); // Remove the executive from the available list
+      clientsToExecutives[socket.id] = assignedExecutive;
+
+      // Notify the client and executive of the connection
+      io.to(socket.id).emit("message", {
+        role: "Support",
+        content: `You have been connected to executive: ${assignedExecutive}`,
+      });
+      io.to(assignedExecutive).emit("message", {
+        role: "Support",
+        content: `You have been assigned a client: ${socket.id}`,
+      });
+    } else {
+    }
+  });
+
+  // Handle messages from clients
   socket.on("message", (msg) => {
-    console.log(msg);
-    io.emit("message", { role: "Support", content: "Customer Support Reply" }); // Broadcast the message to all clients
+    console.log(`Message received from ${socket.id}: ${msg.content}`);
+    if (!availableExecutives.length > 0 && msg.role === "user") {
+      io.to(socket.id).emit("message", {
+        role: "Support",
+        content: "All executives are currently busy. Please wait.",
+      });
+    }
+    // Check if it's a client and send the message to their assigned executive
+    if (clientsToExecutives[socket.id]) {
+      const assignedExecutive = clientsToExecutives[socket.id];
+      io.to(assignedExecutive).emit("message", {
+        role: "Client",
+        content: msg,
+      });
+    }
   });
 
   // Handle disconnection
   socket.on("disconnect", () => {
-    console.log("user disconnected: " + socket.id);
+    console.log(`User disconnected: ${socket.id}`);
+
+    // Check if the disconnected user was an executive or client
+    if (availableExecutives.includes(socket.id)) {
+      // If it was an executive, remove them from the available list
+      availableExecutives = availableExecutives.filter(
+        (execId) => execId !== socket.id
+      );
+      console.log(`Executive ${socket.id} removed from available list`);
+    } else if (clientsToExecutives[socket.id]) {
+      // If it was a client, notify their assigned executive
+      const assignedExecutive = clientsToExecutives[socket.id];
+      io.to(assignedExecutive).emit("message", {
+        role: "Support",
+        content: `Your client ${socket.id} has disconnected.`,
+      });
+
+      // Remove the client from the mapping
+      delete clientsToExecutives[socket.id];
+    }
   });
 });
 
